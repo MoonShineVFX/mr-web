6593a79b0ad5e7d52a960438dd25d308ae19966e
wood <kilokingw@gmail.com>
2022-04-15 19:00:48 +0800

    影片換成YTurl

--- src/App/FbVideo.js
+++ src/App/FbVideo.js
@@ -5,15 +5,15 @@ function FbVideo() {
         <div className="container fbvideo-container">
           <div className="fbvideo-content">
             <iframe 
-                src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FMoondreamReality%2Fvideos%2F272218814545226%2F&width=500&show_text=false&height=282&appId" 
-                width="448" 
-                height="252"
-                style={{border:"none", overflow:"hidden", width:'100%', minHeight:"414px"}}
-                scrolling="no" 
-                frameBorder="0" 
-                allowFullScreen={true}
+              src="https://www.youtube.com/embed/TaPtU2ddLNA" 
+              width="448" 
+              height="252"
+              style={{border:"none", overflow:"hidden", width:'100%', minHeight:"414px"}}
+              scrolling="no" 
+              frameBorder="0" 
+              allowFullScreen
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             >
-
             </iframe>
           </div>
             
