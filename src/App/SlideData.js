b459589449b1594764abf6cebb94fb930215785e
wood.wu <kilokingw@gmail.com>
2022-06-29 21:29:09 +0800

    Update SlideData.js

--- src/App/SlideData.js
+++ src/App/SlideData.js
@@ -33,64 +33,7 @@ const array = [
       '夢想動畫 MoonShine Animation\n'+
       '\n'+
       '<div class="title">導演 Director</div>'+
-      '李元君 Gin Lee\n'+
-      '\n'+
-      '<div class="title">製作總監 Production Supervisor</div>'+
-      '江其諺 Eric Chiang\n'+
-      '\n'+
-      '<div class="title">專案經理 Project Manager</div>'+
-      '王聖鷹 SaintEagle Wang\n'+
-      '吳文琪 Ami Wu\n'+
-      '\n'+
-      '<div class="title">專案助理 Project assistant</div>'+
-      '臺芸萱 Yun Hsuan Tai\n'+
-      '\n'+
-      '<div class="title">模型製作 CG Leader</div>'+
-      '朱家靚 Chia Ching Chu\n'+
-      '\n'+
-      '<div class="title">模型製作 CG Artist</div>'+
-      '周敏雯 Angela Chou\n'+
-      '任永耀 Yung Yao Jen\n'+
-      '\n'+
-      '<div class="title">動作製作 Animation Leader</div>'+
-      '林廷穎 Evan Lin\n'+
-      '\n'+
-      '<div class="title">動作製作 Animator</div>' +
-      '鐘昀麗 Yun Li Chung\n'+
-      '呂佳懋 James Lu\n'+
-      '\n'+
-      '<div class="title">美術設定 Art Designer</div>'+
-      '陳硯詠 Yanyong Chen\n'+
-      '王建盛 Jason Wang\n'+
-      '\n'+
-      '<div class="title">互動技術 Interactive Technician</div>'+
-      '蔡伃晴 A Ching Tsai\n'+
-      '\n'+
-      '<div class="title">技術美術 Technical Artist</div>'+
-      '林柏仲 Mina Lin\n'+
-      '\n'+
-      '<div class="title">場景美術 Scene Artist</div>'+
-      '鄭心怡 Cindy Cheng\n'+
-      '\n'+
-      '<div class="title">動態捕捉 Motion Capture Technician</div>'+
-      '羅志豪 Chih Hao Lo\n'+
-      '邱子杭 Viktor Chiu\n'+
-      '\n'+
-      '<div class="title">音樂製作 Music</div>'+
-      '林承寰 Stan Seven\n'+
-      '\n'+
-      '<div class="title">表演內容</div>'+
-      '藝想台灣劇團 FervorTaiwan\n'+
-      '\n'+
-      '<div class="title">編排/動作設計 Action Designer</div>'+
-      '簡克瑋 Vic Chien\n'+
-      '\n'+
-      '<div class="title">藝術總監 Art Supervisor</div>'+
-      '韓秉融 Vino Han\n'+
-      '\n'+
-      '<div class="title">演員 Actor</div>'+
-      '簡克瑋 Vic Chien\n'+
-      '楊家寧 Ning\n'
+      '李元君 Gin Lee\n'
     ,
     images:['project02/album/act01.jpg' , 'project02/album/act03.jpg']
   },
