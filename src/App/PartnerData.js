65944bd2d144e87eca3d479b868c0124d2b5ede9
wood.wu <kilokingw@gmail.com>
2022-11-17 16:50:01 +0800

    Update PartnerData.js

--- src/App/PartnerData.js
+++ src/App/PartnerData.js
@@ -4,15 +4,6 @@ export default [
         href: 'https://www.moonshine.tw/'
     },
     {
-        images: '/images/ultra_logo.svg',
-        href: 'https://ultracombos.com/'
-    },
-    {
-        images: '/images/fervortaiwan_logo.png',
-        href: 'http://www.fervortaiwan.com/',
-        classname: 'straight'
-    },
-    {
         images: '/images/Optomalogo.png',
         href: 'https://www.optoma.com/tw/'
     },
