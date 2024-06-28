b652a114b51a67804509c4cf9ca635a63e4b229b
wood.wu <kilokingw@gmail.com>
2024-06-29 01:06:15 +0800

    up 1 2

--- src/App/ScheduleData.js
+++ src/App/ScheduleData.js
@@ -1,93 +1,148 @@
 export const data_9 = [
   {
-    title:'九月份場次表',
-    data:[
-      { time:"14:00 - 14:30",	show:"光電音造盤絲洞",	title:"沉浸式投影"},
-      { time:"14:30 - 15:00",	show:"杏仁 ミル",	title:"MR 互動體驗"},
-      { time:"15:00 – 15:30",	show:"杏仁 ミル",	title:"MR 互動體驗"},
-      { time:"16:00 – 16:30",	show:"鶴之舞 + 河馬圓舞曲",	title:"MR 互動體驗"},
-      { time:"17:00 – 17:30",	show:"河馬圓舞曲 + 帝江化生",	title:"MR 互動體驗"},
-      { time:"18:00 – 18:30",	show:"桃符",	title:"沉浸式投影"},
-      { time:"18:30 - 19:00",	show:"光電音造盤絲洞",	title:"沉浸式投影"}
-    ]
+    title: "九月份場次表",
+    data: [
+      { time: "14:00 - 14:30", show: "光電音造盤絲洞", title: "沉浸式投影" },
+      { time: "14:30 - 15:00", show: "杏仁 ミル", title: "MR 互動體驗" },
+      { time: "15:00 – 15:30", show: "杏仁 ミル", title: "MR 互動體驗" },
+      {
+        time: "16:00 – 16:30",
+        show: "鶴之舞 + 河馬圓舞曲",
+        title: "MR 互動體驗",
+      },
+      {
+        time: "17:00 – 17:30",
+        show: "河馬圓舞曲 + 帝江化生",
+        title: "MR 互動體驗",
+      },
+      { time: "18:00 – 18:30", show: "桃符", title: "沉浸式投影" },
+      { time: "18:30 - 19:00", show: "光電音造盤絲洞", title: "沉浸式投影" },
+    ],
   },
   {
-    title:'九月份場次表',
-    data:[
-      { time:"14:00 - 14:30",	show:"光電音造盤絲洞",	title:"沉浸式投影"},
-      { time:"14:30 - 15:00",	show:"杏仁 ミル",	title:"MR 互動體驗"},
-      { time:"15:00 – 15:30",	show:"杏仁 ミル",	title:"MR 互動體驗"},
-      { time:"16:00 – 16:30",	show:"鶴之舞 + 河馬圓舞曲",	title:"MR 互動體驗"},
-      { time:"17:00 – 17:30",	show:"河馬圓舞曲 + 帝江化生",	title:"MR 互動體驗"},
-      { time:"18:00 – 18:30",	show:"桃符",	title:"沉浸式投影"},
-      { time:"18:30 - 19:00",	show:"光電音造盤絲洞",	title:"沉浸式投影"}
-    ]
+    title: "九月份場次表",
+    data: [
+      { time: "14:00 - 14:30", show: "光電音造盤絲洞", title: "沉浸式投影" },
+      { time: "14:30 - 15:00", show: "杏仁 ミル", title: "MR 互動體驗" },
+      { time: "15:00 – 15:30", show: "杏仁 ミル", title: "MR 互動體驗" },
+      {
+        time: "16:00 – 16:30",
+        show: "鶴之舞 + 河馬圓舞曲",
+        title: "MR 互動體驗",
+      },
+      {
+        time: "17:00 – 17:30",
+        show: "河馬圓舞曲 + 帝江化生",
+        title: "MR 互動體驗",
+      },
+      { time: "18:00 – 18:30", show: "桃符", title: "沉浸式投影" },
+      { time: "18:30 - 19:00", show: "光電音造盤絲洞", title: "沉浸式投影" },
+    ],
   },
   {
-    title:'九月份場次表',
-    data:[
-      { time:"14:00 - 14:30",	show:"光電音造盤絲洞",	title:"沉浸式投影"},
-      { time:"14:30 - 15:00",	show:"杏仁 ミル",	title:"MR 互動體驗"},
-      { time:"15:00 – 15:30",	show:"杏仁 ミル",	title:"MR 互動體驗"},
-      { time:"16:00 – 16:30",	show:"鶴之舞 + 河馬圓舞曲",	title:"MR 互動體驗"},
-      { time:"17:00 – 17:30",	show:"河馬圓舞曲 + 帝江化生",	title:"MR 互動體驗"},
-      { time:"18:00 – 18:30",	show:"桃符",	title:"沉浸式投影"},
-      { time:"18:30 - 19:00",	show:"光電音造盤絲洞",	title:"沉浸式投影"}
-    ]
+    title: "九月份場次表",
+    data: [
+      { time: "14:00 - 14:30", show: "光電音造盤絲洞", title: "沉浸式投影" },
+      { time: "14:30 - 15:00", show: "杏仁 ミル", title: "MR 互動體驗" },
+      { time: "15:00 – 15:30", show: "杏仁 ミル", title: "MR 互動體驗" },
+      {
+        time: "16:00 – 16:30",
+        show: "鶴之舞 + 河馬圓舞曲",
+        title: "MR 互動體驗",
+      },
+      {
+        time: "17:00 – 17:30",
+        show: "河馬圓舞曲 + 帝江化生",
+        title: "MR 互動體驗",
+      },
+      { time: "18:00 – 18:30", show: "桃符", title: "沉浸式投影" },
+      { time: "18:30 - 19:00", show: "光電音造盤絲洞", title: "沉浸式投影" },
+    ],
   },
   {
-    title:'九月份場次表',
-    data:[
-      { time:"14:00 - 14:30",	show:"光電音造盤絲洞",	title:"沉浸式投影"},
-      { time:"14:30 - 15:00",	show:"杏仁 ミル",	title:"MR 互動體驗"},
-      { time:"15:00 – 15:30",	show:"杏仁 ミル",	title:"MR 互動體驗"},
-      { time:"16:00 – 16:30",	show:"鶴之舞 + 河馬圓舞曲",	title:"MR 互動體驗"},
-      { time:"17:00 – 17:30",	show:"河馬圓舞曲 + 帝江化生",	title:"MR 互動體驗"},
-      { time:"18:00 – 18:30",	show:"桃符",	title:"沉浸式投影"},
-      { time:"18:30 - 19:00",	show:"光電音造盤絲洞",	title:"沉浸式投影"}
-    ]
-  }
-]
+    title: "九月份場次表",
+    data: [
+      { time: "14:00 - 14:30", show: "光電音造盤絲洞", title: "沉浸式投影" },
+      { time: "14:30 - 15:00", show: "杏仁 ミル", title: "MR 互動體驗" },
+      { time: "15:00 – 15:30", show: "杏仁 ミル", title: "MR 互動體驗" },
+      {
+        time: "16:00 – 16:30",
+        show: "鶴之舞 + 河馬圓舞曲",
+        title: "MR 互動體驗",
+      },
+      {
+        time: "17:00 – 17:30",
+        show: "河馬圓舞曲 + 帝江化生",
+        title: "MR 互動體驗",
+      },
+      { time: "18:00 – 18:30", show: "桃符", title: "沉浸式投影" },
+      { time: "18:30 - 19:00", show: "光電音造盤絲洞", title: "沉浸式投影" },
+    ],
+  },
+];
 
 export const month_data = [
-  {title:'jun_title',index:4},
-  {title:'jul_title',index:4},
-  {title:'aug_title',index:4},
-]
+  { title: "jul_title", index: 4 },
+  { title: "aug_title", index: 4 },
+];
 
 export const data = [
- {
-    id:5,
-    title:'jun_title',
-    subtitle:'jun_subtitle',
-    data:[
-      { time:"14:00",	show:"show_name_9",	title:"guide_category_4"},
-      { time:"15:00",	show:"mar_show_name_3",title:"guide_category_2"},
-      { time:"16:00",	show:"show_name_9",	title:"guide_category_4"},
-      { time:"17:00",	show:"show_name_13",show2:"show_name_10",	title:"guide_category_1",title2:"guide_category_2"},
-      { time:"18:00",	show:"show_name_6",show2:"show_name_10",	title:"guide_category_1",title2:"guide_category_2"},
-    ]
-  }, 
   {
-    id:5,
-    title:'jul_title',
-    subtitle:'jul_subtitle',
-    data:[
-      { time:"11:00、12:30、14:00",	show:"show_name_18",	title:"guide_category_6"},
-      { time:"11:30、13:00、14:30",	show:"show_name_19",title:"guide_category_6"},
-      { time:"12:00、13:30、15:00",	show:"show_name_20",	title:"guide_category_6"},
-      { time:"16:00~18:00",	show:"show_name_13",show2:"show_name_6",	title:"guide_category_7",title2:"guide_category_7"},
-    ]
+    id: 5,
+    title: "jul_title",
+    subtitle: "jul_subtitle",
+    data: [
+      {
+        time: "11:00、12:30、14:00",
+        show: "show_name_18",
+        title: "guide_category_6",
+      },
+      {
+        time: "11:30、13:00、14:30",
+        show: "show_name_19",
+        title: "guide_category_6",
+      },
+      {
+        time: "12:00、13:30、15:00",
+        show: "show_name_20",
+        title: "guide_category_6",
+      },
+      {
+        time: "16:00~18:00",
+        show: "show_name_13",
+        show2: "show_name_6",
+        title: "guide_category_7",
+        title2: "guide_category_7",
+      },
+    ],
   },
   {
-    id:5,
-    title:'aug_title',
-    subtitle:'aug_subtitle',
-    data:[
-      { time:"11:00、12:30、14:00",	show:"show_name_18",	title:"guide_category_6"},
-      { time:"11:30、13:00、14:30",	show:"show_name_19",title:"guide_category_6"},
-      { time:"12:00、13:30、15:00",	show:"show_name_20",	title:"guide_category_6"},
-      { time:"16:00~18:00",	show:"show_name_13",show2:"show_name_6",	title:"guide_category_7",title2:"guide_category_7"},
-    ]
+    id: 5,
+    title: "aug_title",
+    subtitle: "aug_subtitle",
+    data: [
+      {
+        time: "11:00、12:30、14:00",
+        show: "show_name_18",
+        title: "guide_category_6",
+      },
+      {
+        time: "11:30、13:00、14:30",
+        show: "show_name_19",
+        title: "guide_category_6",
+      },
+      {
+        time: "12:00、13:30、15:00",
+        show: "show_name_20",
+        title: "guide_category_6",
+      },
+      {
+        time: "16:00~18:00",
+        show: "show_name_13",
+        show2: "show_name_6",
+        title: "guide_category_7",
+        title2: "guide_category_7",
+      },
+    ],
   },
-]
+];
--- src/App/SlideData.js
+++ src/App/SlideData.js
@@ -1,326 +1,340 @@
 const array = [
   {
-    title: 'slide14_title',
-    subtitle: '懶懶與可可 《第二季：狂歡派對》',
-    description:'slide14_desc_brife',
-    cover: 'project14/cover.jpeg',
-    video:'',
-    reserve_link :'no',
-    ticket_link:'https://www.kkday.com/zh-tw/product/126021?cid=12838',
-    ticket_type:'buy',
-    ticket_link2:'https://play.niceday.tw/zh-tw/regions/21/search?keyword=%E5%A4%A2%E5%A2%83%E7%8F%BE%E5%AF%A6',
-    ticket_type2:'buyfirst',
-    tag: '兒童劇場(雙語互動劇)',
-    creativeidea:'slide14_createive_idea',
+    title: "slide14_title",
+    subtitle: "懶懶與可可 《第二季：狂歡派對》",
+    description: "slide14_desc_brife",
+    cover: "project14/cover.jpeg",
+    video: "https://www.youtube.com/watch?v=mxucXPKt6Ig",
+    reserve_link: "no",
+    ticket_link: "https://www.kkday.com/zh-tw/product/126021?cid=12838",
+    ticket_type: "buy",
+    ticket_link2:
+      "https://play.niceday.tw/zh-tw/regions/21/search?keyword=%E5%A4%A2%E5%A2%83%E7%8F%BE%E5%AF%A6",
+    ticket_type2: "buyfirst",
+    tag: "兒童劇場(雙語互動劇)",
+    creativeidea: "slide14_createive_idea",
     credit:
-      '<div class="title">原創故事</div>'+
-      '夢想動畫 MoonShine Animation \n' +
-      '\n'+
-      '<div class="title">製作公司</div>'+
-      '夢境現實 Moondream Reality \n' +
-      '\n',
-    images:['project14/p01.jpg'],
-    active:true,
-    status:'started'
+      '<div class="title">原創故事</div>' +
+      "夢想動畫 MoonShine Animation \n" +
+      "\n" +
+      '<div class="title">製作公司</div>' +
+      "夢境現實 Moondream Reality \n" +
+      "\n",
+    images: ["project14/p01.jpg"],
+    active: true,
+    status: "started",
   },
   {
-    title: 'slide13_title',
-    subtitle: '森林鬱',
-    description:'slide13_desc_brife',
-    cover: 'project13/cover.jpeg',
-    video:'https://www.youtube.com/watch?v=hegawOcFWhs',
-    reserve_link :'no',
-    ticket_link:'https://www.kkday.com/zh-tw/product/126021?cid=12838',
-    ticket_type:'buy',
-    ticket_link2:'',
-    ticket_type2:'',
-    tag: '沉浸式劇場',
-    creativeidea:'slide13_createive_idea',
+    title: "slide13_title",
+    subtitle: "森林鬱",
+    description: "slide13_desc_brife",
+    cover: "project13/cover.jpeg",
+    video: "https://www.youtube.com/watch?v=hegawOcFWhs",
+    reserve_link: "no",
+    ticket_link: "https://www.kkday.com/zh-tw/product/126021?cid=12838",
+    ticket_type: "buy",
+    ticket_link2: "",
+    ticket_type2: "",
+    tag: "沉浸式劇場",
+    creativeidea: "slide13_createive_idea",
     credit:
-      '<div class="title">製作公司</div>'+
-      '夢想動畫 MoonShine Animation \n' +
-      '\n',
-    images:['project13/p01.jpg'],
-    active:true,
-    status:'holding'
+      '<div class="title">製作公司</div>' +
+      "夢想動畫 MoonShine Animation \n" +
+      "\n",
+    images: ["project13/p01.jpg"],
+    active: true,
+    status: "holding",
   },
   {
-    title: 'slide12_title',
-    subtitle: '夢境V派對',
-    description:'slide12_desc_brife',
-    cover: 'project12/cover.jpeg',
-    video:'https://www.youtube.com/watch?v=FLVxtxZhh48',
-    reserve_link :'no',
-    ticket_link:'https://www.kkday.com/zh-tw/product/126021?cid=12838',
-    ticket_type:'buy_10',
-    ticket_link2:'https://play.niceday.tw/product/7498',
-    ticket_type2:'',
-    tag: 'Vtuber 演唱會',
-    creativeidea:'slide12_createive_idea',
-    credit:"",
-    images:['project12/p01.jpg'],
-    active:false,
-    status:'finished'
+    title: "slide12_title",
+    subtitle: "夢境V派對",
+    description: "slide12_desc_brife",
+    cover: "project12/cover.jpeg",
+    video: "https://www.youtube.com/watch?v=FLVxtxZhh48",
+    reserve_link: "no",
+    ticket_link: "https://www.kkday.com/zh-tw/product/126021?cid=12838",
+    ticket_type: "buy_10",
+    ticket_link2: "https://play.niceday.tw/product/7498",
+    ticket_type2: "",
+    tag: "Vtuber 演唱會",
+    creativeidea: "slide12_createive_idea",
+    credit: "",
+    images: ["project12/p01.jpg"],
+    active: false,
+    status: "finished",
   },
   {
-    title: 'slide11_title',
-    subtitle: '懶懶與可可',
-    description:'slide11_desc_brife',
-    cover: 'project11/cover.jpeg',
-    video:'https://www.youtube.com/watch?v=_IQsLi8qHWI',
-    reserve_link :'no',
-    ticket_link:'https://www.kkday.com/zh-tw/product/126021?cid=12838',
-    ticket_type:'buy',
-    ticket_link2:'https://play.niceday.tw/product/7498',
-    ticket_type2:'buyfirst',
-    tag: '兒童劇場(雙語互動劇)',
-    creativeidea:'slide11_createive_idea',
+    title: "slide11_title",
+    subtitle: "懶懶與可可",
+    description: "slide11_desc_brife",
+    cover: "project11/cover.jpeg",
+    video: "https://www.youtube.com/watch?v=_IQsLi8qHWI",
+    reserve_link: "no",
+    ticket_link: "https://www.kkday.com/zh-tw/product/126021?cid=12838",
+    ticket_type: "buy",
+    ticket_link2: "https://play.niceday.tw/product/7498",
+    ticket_type2: "buyfirst",
+    tag: "兒童劇場(雙語互動劇)",
+    creativeidea: "slide11_createive_idea",
     credit:
-      '<div class="title">原創故事</div>'+
-      '夢想動畫 MoonShine Animation \n' +
-      '\n'+
-      '<div class="title">製作公司</div>'+
-      '夢境現實 Moondream Reality \n' +
-      '\n'
-    ,
-    images:['project11/album/p02.jpg','project11/album/p03.jpg','project11/album/p04.jpg' ],
-    active:true,
-    status:'started'
+      '<div class="title">原創故事</div>' +
+      "夢想動畫 MoonShine Animation \n" +
+      "\n" +
+      '<div class="title">製作公司</div>' +
+      "夢境現實 Moondream Reality \n" +
+      "\n",
+    images: [
+      "project11/album/p02.jpg",
+      "project11/album/p03.jpg",
+      "project11/album/p04.jpg",
+    ],
+    active: true,
+    status: "started",
   },
   {
-    title: 'slide10_title',
-    subtitle: '花慕舞',
-    description:'slide10_desc_brife',
-    cover: 'project10/cover.jpeg',
-    video:'https://www.youtube.com/watch?v=WW1Y7wRNqT4',
-    reserve_link :'no',
-    ticket_link:'https://www.kkday.com/zh-tw/product/126021?cid=12838',
-    ticket_type:'buy',
-    tag: 'MR互動體驗 + 沉浸式劇場',
-    creativeidea:'slide10_createive_idea',
+    title: "slide10_title",
+    subtitle: "花慕舞",
+    description: "slide10_desc_brife",
+    cover: "project10/cover.jpeg",
+    video: "https://www.youtube.com/watch?v=WW1Y7wRNqT4",
+    reserve_link: "no",
+    ticket_link: "https://www.kkday.com/zh-tw/product/126021?cid=12838",
+    ticket_type: "buy",
+    tag: "MR互動體驗 + 沉浸式劇場",
+    creativeidea: "slide10_createive_idea",
     credit:
-      '<div class="title">製作公司</div>'+
-      '夢想動畫 MoonShine Animation \n' +
-      '\n'+
-      '<div class="title">編舞 Dance Design</div>'+
-      ' 小事製作 Les Petites Choses Production\n'+
-      '\n'+
-      '<div class="title">聲音設計 Sound Design</div>'+
-      ' 莉莉周她說 Lily Chou-Chou Lied\n'
-    ,
-    images:['project10/album/p01.jpg','project10/album/p02.jpg' ],
-    active:false,
-    status:'finished'
+      '<div class="title">製作公司</div>' +
+      "夢想動畫 MoonShine Animation \n" +
+      "\n" +
+      '<div class="title">編舞 Dance Design</div>' +
+      " 小事製作 Les Petites Choses Production\n" +
+      "\n" +
+      '<div class="title">聲音設計 Sound Design</div>' +
+      " 莉莉周她說 Lily Chou-Chou Lied\n",
+    images: ["project10/album/p01.jpg", "project10/album/p02.jpg"],
+    active: false,
+    status: "finished",
   },
   {
-    title: 'slide9_title',
-    subtitle: '植物情人',
-    description:'slide9_desc_brife',
-    cover: 'project09/cover.jpg',
-    video:'https://www.youtube.com/watch?v=rapzMIWC8kc',
-    reserve_link :'no',
-    ticket_link:'https://www.kkday.com/zh-tw/product/126021?cid=12838',
-    ticket_type:'buy',
-    tag: '沉浸式劇場',
-    creativeidea:'slide9_createive_idea',
+    title: "slide9_title",
+    subtitle: "植物情人",
+    description: "slide9_desc_brife",
+    cover: "project09/cover.jpg",
+    video: "https://www.youtube.com/watch?v=rapzMIWC8kc",
+    reserve_link: "no",
+    ticket_link: "https://www.kkday.com/zh-tw/product/126021?cid=12838",
+    ticket_type: "buy",
+    tag: "沉浸式劇場",
+    creativeidea: "slide9_createive_idea",
     credit:
-      '<div class="title">製作公司</div>'+
-      '夢想動畫 MoonShine Animation \n' +
-      '大塊文化 Locus Publishing \n' +
-      '\n'+
-      '<div class="title">繪本作者</div>'+
-      ' 黃湘玲\n'
-    ,
-    images:['project09/album/p01.jpg','project09/album/p02.jpg' ],
-    active:true,
-    status:'started'
+      '<div class="title">製作公司</div>' +
+      "夢想動畫 MoonShine Animation \n" +
+      "大塊文化 Locus Publishing \n" +
+      "\n" +
+      '<div class="title">繪本作者</div>' +
+      " 黃湘玲\n",
+    images: ["project09/album/p01.jpg", "project09/album/p02.jpg"],
+    active: true,
+    status: "started",
   },
   {
-    title: 'slide8_title',
-    subtitle: '海與幻',
-    description:'slide8_desc_brife',
-    cover: 'project08/cover.png',
-    video:'https://www.youtube.com/watch?v=8M6nibtLxz4',
-    reserve_link :'no',
-    ticket_link:'https://www.kkday.com/zh-tw/product/126021?cid=12838',
-    ticket_type:'buy',
-    tag: 'MR互動體驗',
-    creativeidea:'slide8_createive_idea',
+    title: "slide8_title",
+    subtitle: "海與幻",
+    description: "slide8_desc_brife",
+    cover: "project08/cover.png",
+    video: "https://www.youtube.com/watch?v=8M6nibtLxz4",
+    reserve_link: "no",
+    ticket_link: "https://www.kkday.com/zh-tw/product/126021?cid=12838",
+    ticket_type: "buy",
+    tag: "MR互動體驗",
+    creativeidea: "slide8_createive_idea",
     credit:
-      '<div class="title">藝術家</div>'+
-      'Kian Khiaban \n' +
-      '\n'+
-      '<div class="title">策展人</div>'+
-      '江宜達\n' + 
-      '\n'+
-      '<div class="title">監製</div>'+
-      '葉孟翰\n' + 
-      '\n'+
-      '<div class="title">製作</div>'+
-      'Future Nature\n' +
-      '\n'+ 
-      '<div class="title">藝術家</div>'+
-      '葉廷皓\n'
-    ,
-    images:['project08/album/p01.jpg','project08/album/p02.jpg' ],
-    active:false,
-    status:'finished'
+      '<div class="title">藝術家</div>' +
+      "Kian Khiaban \n" +
+      "\n" +
+      '<div class="title">策展人</div>' +
+      "江宜達\n" +
+      "\n" +
+      '<div class="title">監製</div>' +
+      "葉孟翰\n" +
+      "\n" +
+      '<div class="title">製作</div>' +
+      "Future Nature\n" +
+      "\n" +
+      '<div class="title">藝術家</div>' +
+      "葉廷皓\n",
+    images: ["project08/album/p01.jpg", "project08/album/p02.jpg"],
+    active: false,
+    status: "finished",
   },
   {
-    title: 'slide7_title',
-    subtitle: '流麻溝十五號 沉浸版',
-    description:'slide7_desc_brife',
-    cover: 'project06/cover.png',
-    video:'https://youtu.be/By0gv9w9NfI',
-    reserve_link :'https://www.kkday.com/zh-tw/product/134815',
-    ticket_link:'https://www.kkday.com/zh-tw/product/134815',
-    ticket_type:'free_reserve',
-    tag: '沉浸式劇場',
-    creativeidea:'slide7_createive_idea',
+    title: "slide7_title",
+    subtitle: "流麻溝十五號 沉浸版",
+    description: "slide7_desc_brife",
+    cover: "project06/cover.png",
+    video: "https://youtu.be/By0gv9w9NfI",
+    reserve_link: "https://www.kkday.com/zh-tw/product/134815",
+    ticket_link: "https://www.kkday.com/zh-tw/product/134815",
+    ticket_type: "free_reserve",
+    tag: "沉浸式劇場",
+    creativeidea: "slide7_createive_idea",
     credit:
-      '<div class="title">製作公司</div>'+
-      '夢想動畫 MoonShine Animation\n' +
-      '湠臺灣電影 thuànnTaiwan\n' 
-    ,
-    images:['project07/album/p01.jpeg','project07/album/p02.jpeg','project07/album/p03.jpeg' ],
-    active:false,
-    status:'finished'
+      '<div class="title">製作公司</div>' +
+      "夢想動畫 MoonShine Animation\n" +
+      "湠臺灣電影 thuànnTaiwan\n",
+    images: [
+      "project07/album/p01.jpeg",
+      "project07/album/p02.jpeg",
+      "project07/album/p03.jpeg",
+    ],
+    active: false,
+    status: "finished",
   },
   {
-    title: 'slide6_title',
-    subtitle: '石虎樂章',
-    description:'slide6_desc_brife',
-    cover: 'project06/cover.png',
-    video:'https://youtu.be/AdtqmzcZyVA',
-    reserve_link :'no',
-    ticket_link:'https://www.kkday.com/zh-tw/product/126021?cid=12838',
-    ticket_type:'buy',
-    tag: 'MR互動沉浸式混合體驗',
-    creativeidea:'slide6_createive_idea',
+    title: "slide6_title",
+    subtitle: "石虎樂章",
+    description: "slide6_desc_brife",
+    cover: "project06/cover.png",
+    video: "https://youtu.be/AdtqmzcZyVA",
+    reserve_link: "no",
+    ticket_link: "https://www.kkday.com/zh-tw/product/126021?cid=12838",
+    ticket_type: "buy",
+    tag: "MR互動沉浸式混合體驗",
+    creativeidea: "slide6_createive_idea",
     credit:
-      '<div class="title">製作公司</div>'+
-      '夢想動畫 MoonShine Animation\n' +
-      '\n'+
-      '<div class="title">導演 Director</div>'+
-      '王舒音 Dolce Wang\n' 
-    ,
-    images:['project06/album/p01.jpg' ],
-    active:true,
-    status:'started'
+      '<div class="title">製作公司</div>' +
+      "夢想動畫 MoonShine Animation\n" +
+      "\n" +
+      '<div class="title">導演 Director</div>' +
+      "王舒音 Dolce Wang\n",
+    images: ["project06/album/p01.jpg"],
+    active: true,
+    status: "started",
   },
   {
-    title: 'slide4_title',
-    subtitle: '河馬',
-    description:'slide4_desc_brife',
-    cover: 'project04/cover.png',
-    video:'https://youtu.be/yPCco2VVzF4',
-    reserve_link :'no',
-    ticket_link:'https://www.kkday.com/zh-tw/product/126021?cid=12838',
-    ticket_type:'buy',
-    tag: 'MR互動沉浸式混合體驗',
-    creativeidea:'slide4_createive_idea',
+    title: "slide4_title",
+    subtitle: "河馬",
+    description: "slide4_desc_brife",
+    cover: "project04/cover.png",
+    video: "https://youtu.be/yPCco2VVzF4",
+    reserve_link: "no",
+    ticket_link: "https://www.kkday.com/zh-tw/product/126021?cid=12838",
+    ticket_type: "buy",
+    tag: "MR互動沉浸式混合體驗",
+    creativeidea: "slide4_createive_idea",
     credit:
-      '<div class="title">製作公司</div>'+
-      '夢想動畫 MoonShine Animation\n' +
-      '\n'+
-      '<div class="title">導演 Director</div>'+
-      '王舒音 Dolce Wang\n' 
-    ,
-    images:['project04/album/p01.jpg' , 'project04/album/p02.jpg', 'project04/album/p03.jpg'],
-    active:true,
-    status:'started'
+      '<div class="title">製作公司</div>' +
+      "夢想動畫 MoonShine Animation\n" +
+      "\n" +
+      '<div class="title">導演 Director</div>' +
+      "王舒音 Dolce Wang\n",
+    images: [
+      "project04/album/p01.jpg",
+      "project04/album/p02.jpg",
+      "project04/album/p03.jpg",
+    ],
+    active: true,
+    status: "started",
   },
   {
     title: "slide2_title",
     subtitle: "Rebirth",
-    description:'slide2_desc_brife',
+    description: "slide2_desc_brife",
     cover: "project02/cover.jpg",
-    video:'https://youtu.be/nERe6qYmMJc',
-    reserve_link :'https://www.klook.com/zh-TW/',
-    ticket_link:'https://www.kkday.com/zh-tw/product/126021?cid=12838',
-    ticket_type:'buy',
-    tag: 'MR互動沉浸式混合體驗',
-    creativeidea:'slide2_createive_idea',
+    video: "https://youtu.be/nERe6qYmMJc",
+    reserve_link: "https://www.klook.com/zh-TW/",
+    ticket_link: "https://www.kkday.com/zh-tw/product/126021?cid=12838",
+    ticket_type: "buy",
+    tag: "MR互動沉浸式混合體驗",
+    creativeidea: "slide2_createive_idea",
     credit:
-      '<div class="title">製作公司</div>'+
-      '夢想動畫 MoonShine Animation\n'+
-      '\n'+
-      '<div class="title">導演 Director</div>'+
-      '李元君 Gin Lee\n'
-    ,
-    images:['project02/album/act01.jpg' , 'project02/album/act03.jpg'],
-    active:true,
-    status:'started'
+      '<div class="title">製作公司</div>' +
+      "夢想動畫 MoonShine Animation\n" +
+      "\n" +
+      '<div class="title">導演 Director</div>' +
+      "李元君 Gin Lee\n",
+    images: ["project02/album/act01.jpg", "project02/album/act03.jpg"],
+    active: true,
+    status: "started",
   },
   {
-    title: 'slide3_title',
-    subtitle: 'Flamingo',
-    description:'slide3_desc_brife',
-    cover: 'project03/cover.png',
-    video:'https://youtu.be/DRgQxNZGLGE',
-    reserve_link :'no',
-    ticket_link:'https://www.kkday.com/zh-tw/product/126021?cid=12838',
-    ticket_type:'buy',
-    tag: 'MR互動沉浸式混合體驗',
-    creativeidea:'slide3_createive_idea',
+    title: "slide3_title",
+    subtitle: "Flamingo",
+    description: "slide3_desc_brife",
+    cover: "project03/cover.png",
+    video: "https://youtu.be/DRgQxNZGLGE",
+    reserve_link: "no",
+    ticket_link: "https://www.kkday.com/zh-tw/product/126021?cid=12838",
+    ticket_type: "buy",
+    tag: "MR互動沉浸式混合體驗",
+    creativeidea: "slide3_createive_idea",
     credit:
-      '<div class="title">製作公司</div>'+
-      '夢想動畫 MoonShine Animation\n' +
-      '\n'+
-      '<div class="title">導演 Director</div>'+
-      '王舒音 Dolce Wang\n'
-    ,
-    images: ['project03/album/p01.jpg' , 'project03/album/p02.jpg', 'project03/album/p03.jpg'],
-    active:true,
-    status:'started'
+      '<div class="title">製作公司</div>' +
+      "夢想動畫 MoonShine Animation\n" +
+      "\n" +
+      '<div class="title">導演 Director</div>' +
+      "王舒音 Dolce Wang\n",
+    images: [
+      "project03/album/p01.jpg",
+      "project03/album/p02.jpg",
+      "project03/album/p03.jpg",
+    ],
+    active: true,
+    status: "started",
   },
   {
-    title: 'slide5_title',
-    subtitle: '盤絲洞',
-    description:'slide5_desc_brife',
-    cover: 'project05/cover.png',
-    video:'https://youtu.be/jqxXyWwlreE',
-    reserve_link :'no',
-    ticket_link:'https://www.kkday.com/zh-tw/product/126021?cid=12838',
-    ticket_type:'buy',
-    tag: '沉浸式投影',
-    creativeidea:'slide5_createive_idea',
+    title: "slide5_title",
+    subtitle: "盤絲洞",
+    description: "slide5_desc_brife",
+    cover: "project05/cover.png",
+    video: "https://youtu.be/jqxXyWwlreE",
+    reserve_link: "no",
+    ticket_link: "https://www.kkday.com/zh-tw/product/126021?cid=12838",
+    ticket_type: "buy",
+    tag: "沉浸式投影",
+    creativeidea: "slide5_createive_idea",
     credit:
-      '<div class="title">製作公司</div>'+
-      '光電吉樹\n' +
-      '\n'+
-      '<div class="title">導演 Director</div>'+
-      '劉育樹\n' 
-    ,
-    images:['project05/album/p01.jpg' , 'project05/album/p02.jpg', 'project05/album/p03.jpg'],
-    active:false,
-    status:'finished'
+      '<div class="title">製作公司</div>' +
+      "光電吉樹\n" +
+      "\n" +
+      '<div class="title">導演 Director</div>' +
+      "劉育樹\n",
+    images: [
+      "project05/album/p01.jpg",
+      "project05/album/p02.jpg",
+      "project05/album/p03.jpg",
+    ],
+    active: false,
+    status: "finished",
   },
   {
-    title: 'slide1_title',
-    subtitle: 'Tao Fu',
-    description:'slide1_desc_brife',
-    cover: 'project01/cover.png',
-    video:'https://youtu.be/x0rxfRCBZPU',
-    reserve_link :'no',
-    ticket_link:'https://www.kkday.com/zh-tw/product/126021?cid=12838',
-    ticket_type:'buy',
-    tag: '沉浸式投影',
-    creativeidea:'slide1_createive_idea',
+    title: "slide1_title",
+    subtitle: "Tao Fu",
+    description: "slide1_desc_brife",
+    cover: "project01/cover.png",
+    video: "https://youtu.be/x0rxfRCBZPU",
+    reserve_link: "no",
+    ticket_link: "https://www.kkday.com/zh-tw/product/126021?cid=12838",
+    ticket_type: "buy",
+    tag: "沉浸式投影",
+    creativeidea: "slide1_createive_idea",
     credit:
-      '<div class="title">製作公司</div>'+
-      '叁式 Ultra Combos\n' +
-      '\n'+
-      '<div class="title">聲音設計 Sound Design</div>'+
-      '黃鎮洋 Zhen-Yang Huang (Triodust)\n' +
-      '\n'+
-      '<div class="title">弦樂 Strings</div>'+
-      '龍俊宇 Chun-Yu Lung (Joy Music)\n'
-    ,
-    images:['project01/album/mobi_act02.jpg' , 'project01/album/mobi_act03.jpg', 'project01/album/mobi_act04.jpg'],
-    active:false,
-    status:'finished'
-  }
+      '<div class="title">製作公司</div>' +
+      "叁式 Ultra Combos\n" +
+      "\n" +
+      '<div class="title">聲音設計 Sound Design</div>' +
+      "黃鎮洋 Zhen-Yang Huang (Triodust)\n" +
+      "\n" +
+      '<div class="title">弦樂 Strings</div>' +
+      "龍俊宇 Chun-Yu Lung (Joy Music)\n",
+    images: [
+      "project01/album/mobi_act02.jpg",
+      "project01/album/mobi_act03.jpg",
+      "project01/album/mobi_act04.jpg",
+    ],
+    active: false,
+    status: "finished",
+  },
 ];
 
 export default array;
